5ea4c86520ca8fd41655b7d4c63424094840f901
Diligent ElZee <el_zee@zoho.com>
2025-03-22 21:33:09 +0100

    Initial commit

---
+++ src/main/java/net/diligentelzee/furrylounge/FurryLoungeClient.java
@@ -0,0 +1,10 @@
+package net.diligentelzee.furrylounge;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FurryLoungeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
